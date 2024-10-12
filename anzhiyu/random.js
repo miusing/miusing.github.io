c1651db2496bae17003f8808a6bcff758c48c55f
miusing <3059233286@qq.com>
2024-10-12 15:40:10 +0800

    Site updated: 2024-10-12 15:40:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello-world/","测试文章/"];function toRandomPost(){
+var posts=["hello-world/","关于我/","测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
