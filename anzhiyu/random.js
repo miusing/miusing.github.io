1ad6528d253dd9c7d604119e3c8702113125fda2
miusing <3059233286@qq.com>
2024-10-12 15:49:38 +0800

    Site updated: 2024-10-12 15:49:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["hello-world/","关于我/","测试文章/"];function toRandomPost(){
+var posts=["关于我/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
