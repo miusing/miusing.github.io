1c601763da23e990a2c217ef3530fe035727cb62
miusing <3059233286@qq.com>
2024-10-13 17:04:53 +0800

    Site updated: 2024-10-13 17:04:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["test-post/"];function toRandomPost(){
+var posts=["为什么要读书？——探索阅读的真正意义与价值/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
