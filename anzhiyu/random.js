09df767534105c3418f0857df1c15f412cf7d620
miusing <3059233286@qq.com>
2024-10-14 17:58:03 +0800

    Site updated: 2024-10-14 17:58:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["第一章《价格行为基础知识-图表基础》/","为什么要读书？——探索阅读的真正意义与价值/"];function toRandomPost(){
+var posts=["为什么要读书？——探索阅读的真正意义与价值/","第一章《价格行为基础知识-图表基础》/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
