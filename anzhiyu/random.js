13703d04ebd075930ad0245264ea7032f3eef00b
miusing <3059233286@qq.com>
2024-10-15 23:17:44 +0800

    Site updated: 2024-10-15 23:17:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["为什么要读书？——探索阅读的真正意义与价值/","用 AI 学 Python 编程：从入门到进阶的成长路线/","《一起用AI学Python编程》都有啥服务？/","第一章《价格行为基础知识-图表基础》/","《一起用AI学Python编程》课程介绍/"];function toRandomPost(){
+var posts=["《一起用AI学Python编程》课程介绍/","为什么要读书？——探索阅读的真正意义与价值/","《一起用AI学Python编程》都有啥服务？/","用 AI 学 Python 编程：从入门到进阶的成长路线/","第一章《价格行为基础知识-图表基础》/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
